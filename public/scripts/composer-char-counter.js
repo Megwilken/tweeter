c6c62c878db36fda3dee6ae121f5c05ad5326f7b
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-01-24 23:29:37 -0600

    add new-tweet css file and character counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
-console.log("hey")
+  console.log("hey")
+  $('#tweet-text').keyup(function () {
+  const chars = $(this).val().length;
+  const charsLeft = 140 - chars;
+  if (charsLeft < 0) {
+ 
+    $('#charCount').text(charsLeft).addClass("charsNeg");
+  } else {
+    $('#charCount').text(charsLeft);
+  }
+});
+
+
 $(".text").on("input", function() {
 console.log("type!")
 });
+
+$(".button").on("submit", function() {
+  console.log("submit!")
+  });
 });
