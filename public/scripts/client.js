0f78e5082c446b331939ba0cc678ae425f28e342
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-01-26 23:11:05 -0600

    use Object.keys in createTweetElement function to debug console errors (tweets is not iterable)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,11 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {
+$(document).ready(function() {
   console.log("jQuery is ready");
 
   const tweetData = {
-    user: {
+     user: {
       name: "Newton",
       avatars: "https://i.imgur.com/73hZDYK.png",
       handle: "@SirIsaac",
@@ -17,12 +17,12 @@ $(document).ready(function () {
       text: "If I have seen further it is by standing on the shoulders of giants",
     },
     created_at: 1461116232227,
-    
   };
 
-
+  
   const createTweetElement = function(tweet) {
-    const $tweet = `<article>
+    let $tweet = $("<article>").addClass("tweet");
+    const tweetBody = `
        <img src=${tweetData.user.avatars}>
        <h2>${tweetData.user.name}</h2>
        <h2>${tweetData.user.handle}</h2>
@@ -31,17 +31,19 @@ $(document).ready(function () {
        <i class="fa-solid fa-flag" id="footerIcon"></i>
        <i class="fa-solid fa-retweet" id="footerIcon"></i>
        <i class="fa-solid fa-heart" id="footerIcon"></i>
-       </article>`;
-       console.log($tweet)
+       `;
+
+    $tweet.append(tweetBody)
     return $tweet;
-  
   };
 
-  const renderTweets = function (tweets) {
-    for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
+  const renderTweets = function(tweets) {
+    for (const tweet of Object.keys(tweets)) {
+     let $tweet = createTweetElement(tweet);
       $(`#tweets-container`).append($tweet);
     }
-    renderTweets(tweets);
   };
+  renderTweets(tweetData);
+
 });
+    
