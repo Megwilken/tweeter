47d683f6803a9f55de6343b7018523a6d7c37c59
Meg Wilken <102841984+Megwilken@users.noreply.github.com>
2023-01-28 10:26:48 -0600

    implement form submission using jQuery - further work to be completed to postTweet function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,11 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("jQuery is ready");
 
   const tweetData = {
-     user: {
+    user: {
       name: "Newton",
       avatars: "https://i.imgur.com/73hZDYK.png",
       handle: "@SirIsaac",
@@ -19,31 +19,61 @@ $(document).ready(function() {
     created_at: 1461116232227,
   };
 
-  
   const createTweetElement = function(tweet) {
     let $tweet = $("<article>").addClass("tweet");
     const tweetBody = `
-       <img src=${tweetData.user.avatars}>
-       <h2>${tweetData.user.name}</h2>
-       <h2>${tweetData.user.handle}</h2>
-       <p>${tweetData.content.text}</p>
-       <p>${tweetData.created_at}</p>
-       <i class="fa-solid fa-flag" id="footerIcon"></i>
+    <header>
+       <span>
+       <h2 class="userNameTweetBox"><img src=${tweetData.user.avatars}>${tweetData.user.name}</h2>
+       </span>
+       <span>
+       <h2 class="userHandle">${tweetData.user.handle}</h2>
+       </span>
+       </header>
+       <main>
+       <p class="userTweet">${tweetData.content.text}</p>
+       <hr>
+       </main>
+       <footer>
+       <span>
+       <p class="datePosted">${tweetData.created_at}</p>
+       </span>
+       <span><i class="fa-solid fa-flag" id="footerIcon"></i>
        <i class="fa-solid fa-retweet" id="footerIcon"></i>
-       <i class="fa-solid fa-heart" id="footerIcon"></i>
+       <i class="fa-solid fa-heart" id="footerIcon"></i></span>
+       </footer>
        `;
 
-    $tweet.append(tweetBody)
+    $tweet.append(tweetBody);
     return $tweet;
   };
 
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     for (const tweet of Object.keys(tweets)) {
-     let $tweet = createTweetElement(tweet);
+      let $tweet = createTweetElement(tweet);
       $(`#tweets-container`).append($tweet);
     }
   };
   renderTweets(tweetData);
 
-});
-    
+$("#submit").submit(function(event) {
+  event.preventDefault();
+  alert("submitted");
+  postTweet()
+})
+
+const postTweet = () => {
+  $.ajax({
+    url: "/tweets/", 
+    type: "POST",
+    data: $('.new-tweet').serialize(),
+    success: (data) => {
+      console.log(data);
+      $("#submit").prop("disabled", false).text("Submit");
+    renderTweets()
+//     }
+//   });
+//     }
+//   })
+  
+
